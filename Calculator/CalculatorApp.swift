658bcb4ee502af3defaa0391b4320fefac5d4f85
Fezz <jonnicwolf@gmail.com>
2024-02-27 18:20:56 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jonathan Narine on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
